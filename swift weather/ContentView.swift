3e146e5502178820a3887be6e0b7b5e1e90c6bd6
vishalx06 <vishalx06@gmail.com>
2024-03-18 09:31:29 +0530

    Initial Commit

---
+++ swift weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift weather
+//
+//  Created by Vishal Sharma on 18/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift weather/swift_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_weatherApp.swift
+//  swift weather
+//
+//  Created by Vishal Sharma on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
