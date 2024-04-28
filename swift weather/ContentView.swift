c1153a1b164a5ecb8773786513b382675e022d71
vishalx06 <vishalx06@gmail.com>
2024-04-28 17:50:26 +0530

    first commit

--- swift weather/ContentView.swift
+++ swift weather/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
